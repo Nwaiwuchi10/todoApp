3810280683a413eaa56c541f0d35ce66008a3938
Nwaiwuchi10 <djnchrys@gmail.com>
2025-08-21 02:24:44 +0100

    new

--- src/routes/Todo.ts
+++ src/routes/Todo.ts
@@ -17,13 +17,13 @@ const router = Router();
 
 router.post("/", createTodo);
 router.get("/", getAllTodos);
-router.get("/:id", getTodoById);
+router.get("/:id/data", getTodoById);
 router.get("/slug/:slug", getTodoBySlug);
 router.put("/:id", updateTodo);
 router.delete("/:id", deleteTodo);
 router.patch("/:id/status", updateStatus);
 router.patch("/:id/comment", updateComment);
-router.get("/:id", getTodoWithComments);
+router.get("/:id/", getTodoWithComments);
 
 // Filter projects by due dates (?from=2025-08-01&to=2025-08-30)
 router.get("/filter/due-dates", getTodosByDueDate);
